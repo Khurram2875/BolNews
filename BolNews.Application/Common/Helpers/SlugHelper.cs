dd09e8d55f5ce1d3cb8de3801576edc183de2d83
Khurram <khurram2875@gmail.com>
2026-04-14 22:38:12 +0500

    SEO System Implementation
    -Added SlugHelpers

---
+++ BolNews.Application/Common/Helpers/SlugHelper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace BolNews.Application.Common.Helpers
+{
+    public static class SlugHelper
+    {
+        public static string GenerateSlug(string phrase)
+        {
+            string str = phrase.ToLower();
+
+            // Remove invalid chars
+            str = Regex.Replace(str, @"[^a-z0-9\s-]", "");
+
+            // Convert multiple spaces into one space
+            str = Regex.Replace(str, @"\s+", " ").Trim();
+
+            // Replace spaces with hyphens
+            str = str.Replace(" ", "-");
+
+            return str;
+        }
+    }
+}
