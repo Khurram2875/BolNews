6091728ecdcb817de8c67fbfe64f2459b9bebeb3
Khurram2875 <87568943+Khurram2875@users.noreply.github.com>
2026-09-23 00:41:45 +0500

    Add trending date-range service contract

--- BolNews.Application/Interfaces/ITrendingService.cs
+++ BolNews.Application/Interfaces/ITrendingService.cs
@@ -5,5 +5,6 @@ namespace BolNews.Application.Interfaces
     public interface ITrendingService
     {
         Task<List<TrendingTopicResult>> GetTrendingTopicsAsync();
+        Task<List<TrendingTopicResult>> GetTrendingTopicsAsync(DateTime fromDate, DateTime toDate);
     }
 }
