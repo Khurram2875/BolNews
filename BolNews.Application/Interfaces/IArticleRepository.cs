af2051c69ec7cb3f68c53ff2834d7cd521cff46b
Khurram2875 <87568943+Khurram2875@users.noreply.github.com>
2026-09-23 00:48:16 +0500

    Add total published article count repository contract

--- BolNews.Application/Interfaces/IArticleRepository.cs
+++ BolNews.Application/Interfaces/IArticleRepository.cs
@@ -39,6 +39,7 @@ namespace BolNews.Application.Interfaces
         Task<List<Article>> GetLowPerformingAsync(DateTime since, int maxViews, int limit);
         Task<List<Article>> GetLowPerformingAsync(DateTime fromDate, DateTime toDate, int maxViews, int limit);
         Task<int> CountAsync();
+        Task<int> CountPublishedAsync();
         Task<int> CountPublishedSinceAsync(DateTime since);
         Task<int> CountPublishedBetweenAsync(DateTime fromDate, DateTime toDate);
         Task<List<(DateTime Date, int Count)>> CountPerDayAsync(DateTime fromDate, DateTime toDate);
